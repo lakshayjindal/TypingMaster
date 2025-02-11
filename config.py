bf85d13f0b870cc03982222bafe01f9f26d83556
Lakshay Jindal <ljindal275@gmail.com>
2025-02-11 12:41:47 +0000

    Corrected the Database URL

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:s2wYBXaYeEdGcae5@db.pnrsjvznrgssigosrqsl.supabase.co:6543/postgres"
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:s2wYBXaYeEdGcae5@db.pnrsjvznrgssigosrqsl.supabase.co:5432/postgres"
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:IpVJuBOZvPcqftoCudStPoRbEnZORDog@monorail.proxy.rlwy.net:12026/railway"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
